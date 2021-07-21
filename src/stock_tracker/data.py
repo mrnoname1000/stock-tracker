8a1e193fb3f9a23f2c23fd228ed473cebd622e5a
Oliver Nutter <mrnoname1000@riseup.net>
2021-07-20 23:46:10 -0500

    Improve formatting with the help of black

--- src/stock_tracker/data.py
+++ src/stock_tracker/data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 def obv(ticker, period="1mo"):
     """
     Calculate OBV for a stock
--- src/stock_tracker/option.py
+++ src/stock_tracker/option.py
@@ -8,39 +8,46 @@ def positive_int(i):
         raise ValueError
     return int(i)
 
+
 def build_parser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter,
-        description="rank stocks based on various factors",
+        description="Rank stocks based on various factors",
     )
 
     parser.add_argument(
         "stocks",
         metavar="STOCK",
         nargs="*",
-        help="manually specify stocks to search",
+        help="Manually specify stocks to search",
     )
 
     parser.add_argument(
         "--market-cap-min",
+        metavar="INT",
         action="store",
         default=50000,
         type=int,
-        help=textwrap.dedent("""\
-            minimum market cap to search
+        help=textwrap.dedent(
+            """
+            Minimum market cap to search
             default: %(default)s
-        """),
+            """
+        ).strip("\n"),
     )
 
     parser.add_argument(
         "--market-cap-max",
+        metavar="INT",
         action="store",
         default=None,
         type=positive_int,
-        help=textwrap.dedent("""\
-            maximum market cap to search
+        help=textwrap.dedent(
+            """
+            Maximum market cap to search
             default: none
-        """),
+            """
+        ).strip("\n"),
     )
 
     date_values = ("1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max")
@@ -50,10 +57,12 @@ def build_parser():
         action="store",
         choices=date_values,
         default="max",
-        help=textwrap.dedent(f"""\
-            period of dates to consider
+        help=textwrap.dedent(
+            f"""
+            Period of dates to consider (e.g. 5d, 6mo, 10y, ytd, max)
             default: %(default)s
-        """),
+            """
+        ).strip("\n"),
     )
 
     return parser
