a386ba6bfbf37fe2febc8dc88cf766d452907485
Oliver Nutter <mrnoname1000@riseup.net>
2021-11-29 13:02:28 -0600

    Remove score function

--- src/stock_tracker/data.py
+++ src/stock_tracker/data.py
@@ -22,21 +22,3 @@ def obv(ticker, period="1mo", interval="1d"):
 
 def zero_if_nan(x):
     return np.where(np.isnan(x), 0, x)
-
-
-def score(df):
-    score = 0
-    # 1
-    score += zero_if_nan(df.earningsQuarterlyGrowth)
-    # 2
-    score += zero_if_nan(df.revenueGrowth)
-    # 3
-    score += zero_if_nan(df.pegRatio - 1)
-    # 4
-    score += zero_if_nan(df.grossMargins - 0.35)
-    # 5
-    score -= zero_if_nan(df.debtToEquity / 100 - 0.35)
-    # 7
-    score += zero_if_nan(df.trailingPE) / df.forwardPE
-
-    return score
