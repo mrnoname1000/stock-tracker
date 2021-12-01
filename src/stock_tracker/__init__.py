f21d5e94c836582bd03847ee7c7b4f6bf84b84de
Oliver Nutter <mrnoname1000@riseup.net>
2021-12-01 14:36:07 -0600

    Cleanup main

--- src/stock_tracker/__init__.py
+++ src/stock_tracker/__init__.py
@@ -10,8 +10,6 @@ from . import constants, option, data, threading, yahoo
 def main():
     opts = option.build_parser().parse_args()
 
-    stocks = pd.DataFrame(index=opts.stocks)
-
     # pandas config
     pd.set_option("display.float_format", lambda x: f"{x:f}")
     pd.set_option("display.max_rows", None)
@@ -20,7 +18,6 @@ def main():
 
     # get yahoo calendar data
     earnings = yahoo.get_stock_earnings_data_between(opts.start, opts.end)
-
 
     for ticker, df in earnings.items():
         yahoo.populate_earnings_df(ticker, df)
