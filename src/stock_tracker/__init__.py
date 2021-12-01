2ea2b4684bd2450d7c972832d1701b2b422b248a
Oliver Nutter <mrnoname1000@riseup.net>
2021-12-01 12:13:03 -0600

    main(): Remove checks for opts.stocks and opts.lookup
    - Remove unused YahooEarningsCalendar init

--- src/stock_tracker/__init__.py
+++ src/stock_tracker/__init__.py
@@ -18,16 +18,9 @@ def main():
     pd.set_option("display.max_columns", None)
 
 
-    # get yahoo earnings calendar
-    yec = YahooEarningsCalendar()
-
-    # get yahoo calendar data if required
-    if not opts.stocks:
-        opts.lookup = True
-
-    if opts.lookup:
-        earnings = yahoo.get_stock_earnings_data_between(opts.start, opts.end)
-        stocks = pd.concat([stocks, earnings])
+    # get yahoo calendar data
+    earnings = yahoo.get_stock_earnings_data_between(opts.start, opts.end)
+    stocks = pd.concat([stocks, earnings])
 
     stocks = pd.concat([stocks, yahoo.get_stock_data(*stocks.index)])
     print(stocks)
