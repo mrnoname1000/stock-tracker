08d34fce4b6633812967fe633918198c6d148aae
Oliver Nutter <mrnoname1000@riseup.net>
2021-12-02 14:53:03 -0600

    option: Remove unused time period code

--- src/stock_tracker/option.py
+++ src/stock_tracker/option.py
@@ -1,38 +1,6 @@
 import argparse, datetime
 
 
-def period(s):
-    """Test for valid time period"""
-    lower = s.lower()
-
-    if lower in ("ytd", "max"):
-        return s
-
-    for suffix in ("d", "mo", "y"):
-        prefix = s[: -len(suffix)]
-        if lower.endswith(suffix) and prefix.encode("ascii", errors="replace").isdigit():
-            return s
-
-    raise ValueError
-
-
-INTERVAL_VALUES = (
-    "1m",
-    "2m",
-    "5m",
-    "15m",
-    "30m",
-    "60m",
-    "90m",
-    "1h",
-    "1d",
-    "5d",
-    "1wk",
-    "1mo",
-    "3mo",
-)
-
-
 def positive_int(i):
     if i < 0:
         raise ValueError
