3d51b329de0c87ce74a33e8be717392ef93328ed
Oliver Nutter <mrnoname1000@riseup.net>
2022-08-29 04:28:16 -0500

    Add flask app

---
+++ stock_tracker/flask.py
@@ -0,0 +1,51 @@
+import re, traceback
+
+from flask import Flask, request, render_template, Blueprint
+from flask_htmlmin import HTMLMIN
+
+try:
+	from jinja2.utils import markupsafe
+	Markup = markupsafe.Markup
+	escape = markupsafe.escape
+	from jinja2 import pass_eval_context as evalcontextfilter
+except ImportError:
+	from jinja2 import evalcontextfilter
+	from jinja2 import Markup, escape
+
+from . import yahoo, data
+from .constants import PROGNAME
+
+app = Flask(PROGNAME, template_folder="stock_tracker/templates")
+app.config['MINIFY_HTML'] = True
+htmlmin = HTMLMIN(app)
+
+@app.route("/")
+def index():
+    tickers = request.args.get("tickers", "").replace(",", " ").split()
+    stocks = [yahoo.Stock(t) for t in tickers]
+    status = {}
+    try:
+        for stock in stocks:
+            status[stock.ticker] = data.evaluate(stock)
+    except Exception:
+        error = traceback.format_exc()
+        print(error)
+    else:
+        error = None
+    return render_template("index.html", tickers=status, error=error)
+
+
+blueprint = Blueprint('custom_template_filters', __name__)
+
+
+@evalcontextfilter
+@blueprint.app_template_filter()
+def nl2br(context, value: str) -> str:
+    result = "<br />".join(re.split(r'(?:\r\n|\r|\n){2,}', escape(value)))
+
+    if context.autoescape:
+        result = Markup(result)
+
+    return result
+
+app.register_blueprint(blueprint)
