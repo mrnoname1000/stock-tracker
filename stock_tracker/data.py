99039e400ce69f8e5117cbf69432e702b5efd077
Oliver Nutter <mrnoname1000@riseup.net>
2022-08-29 02:32:54 -0500

    data: Remove unused functions

--- stock_tracker/data.py
+++ stock_tracker/data.py
@@ -2,29 +2,6 @@ import numpy as np
 import pandas as pd
 
 
-def obv(ticker, period="1mo", interval="1d"):
-    """
-    Calculate OBV for a stock
-
-    Arguments:
-        ticker: yfinance Ticker object
-        period: same as yfinance.history()
-        interval: same as yfinance.history()
-
-    Return:
-        an integer
-    """
-    df = ticker.history(period=period, interval=interval)
-    if len(df.index) <= 1:
-        return 0
-
-    return (np.sign(df.Close.diff()) * df.Volume).cumsum().values[-1]
-
-
-def zero_if_nan(x):
-    return np.where(np.isnan(x), 0, x)
-
-
 def evaluate(stock):
     ec = stock.earnings_calendar
 
